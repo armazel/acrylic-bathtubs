72cee1f6ef814ba9c458871ce098611761473aed
Egor <dragun.egor@gmail.com>
2016-11-20 19:28:52 +0300

    add slick slide

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slick-banner-header').slick({
+        dots:true,
+        slidesToScroll: 1,
+        autoplay: true,
+        touchMove:true,
+
+        prevArrow:false,
+        nextArrow:false,
+        autoplaySpeed: 6000
+});
+});
