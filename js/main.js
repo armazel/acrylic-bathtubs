59f50d9e8a9dd2828b6629284dd7addea690e586
Egor <dragun.egor@gmail.com>
2017-03-19 23:46:04 +0300

    new block

--- js/main.js
+++ js/main.js
@@ -8,6 +8,38 @@ $(document).ready(function(){
         autoplaySpeed: 6000
 });
 
+    $('.slick-examples').slick({
+        touchMove:true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
     $("#arrow-info").on("click", function (event) {
         event.preventDefault();
 
@@ -17,6 +49,11 @@ $(document).ready(function(){
 
         $('body,html').animate({scrollTop: top-70}, 1500);
     });
+
+
 });
+
+
+
 
 
